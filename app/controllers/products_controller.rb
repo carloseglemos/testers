844410cc232eb4eaa9fbabf831fcbfe102e5a6c3
Carlos Lemos <carloseduardolemos@hotmail.com>
2022-04-26 12:39:52 -0300

    tdd

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
     skip_before_action :authenticate_user!, only: :index
 
-    def index
-      @products = Product.all
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products, only: %i[new create]
   devise_for :users
   root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
